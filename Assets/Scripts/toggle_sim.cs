277c8ccb05d7370bf7e0822a36e27ec3206f768a
jrugis <j.rugis@auckland.ac.nz>
2022-03-11 01:41:52 +1300

    back to all discs

---
+++ Assets/Scripts/toggle_sim.cs
@@ -0,0 +1,24 @@
+﻿/*
+Parotid Simulation 
+Attach this to a MiniGland object.
+*/
+using System;
+using System.Text;
+using UnityEngine;
+public class toggle_sim : MonoBehaviour
+{
+    public bool simulate;
+    private void toggle()
+    {
+        simulate = !simulate;
+    }
+    void Start()
+    {
+        simulate = false;
+    }
+
+    void Update ()
+    {  
+        if (Input.GetKeyDown (KeyCode.Space)) toggle();
+    }  
+}
